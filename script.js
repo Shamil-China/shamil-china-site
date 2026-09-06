53ba1f8c6fd6aa5258b7b2e6c492a86f6f17160a
Shamil-China <n67ttsjpkd@privaterelay.appleid.com>
2026-09-07 00:29:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1025,16 +1025,3 @@ async function startStore() {
 
 
 startStore();
-html {
-  background: #F7F3EB;
-}
-
-body {
-  background: #F7F3EB;
-  padding-top: env(safe-area-inset-top);
-}
-
-.app-splash {
-  padding-top: env(safe-area-inset-top);
-}
-
